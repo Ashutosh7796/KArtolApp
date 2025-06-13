ef961d53021809a69c315c44861b98df2dfa3baa
tkraj888 <tusharck88@gmail.com>
2025-06-13 18:52:52 +0530

    exam entity

---
+++ src/main/java/com/spring/jwt/Exam/Dto/ExamSessionDTO.java
@@ -0,0 +1,17 @@
+package com.spring.jwt.Exam.Dto;
+
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ExamSessionDTO {
+    private Integer sessionId;
+    private Integer userId;
+    private Integer paperId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Integer score;
+    private List<UserAnswerDTO> userAnswers;
+}
---
+++ src/main/java/com/spring/jwt/Exam/Dto/PaperQuestionDTO.java
@@ -0,0 +1,11 @@
+package com.spring.jwt.Exam.Dto;
+
+import lombok.Data;
+
+@Data
+public class PaperQuestionDTO {
+    private Integer id;
+    private Integer paperId;
+    private Integer questionId;
+    private Integer questionOrder;
+}
